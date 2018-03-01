dd48f39a857a61f6cbf04ad02f780cbb5a84a0d1
Derek Pham <anhdpham97@gmail.com>
2018-02-28 21:34:55 -0500

    Rofi: enabled arrow keys; aliases: python=python2 for django; xmonad: add alt-tab, cycling workspaces

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,17 +1,51 @@
+-- TODO:
+-- +) Clean out imports
+
 import XMonad
 import XMonad.Config.Xfce
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 import System.IO
+
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing (spacing)
+import XMonad.Layout.Minimize
+
+import XMonad.Actions.CopyWindow
+import XMonad.Actions.CycleWS
+import XMonad.Actions.CycleWindows
+import XMonad.Actions.GroupNavigation
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
 main = do
   xmonad $ xfceConfig
     {
-      manageHook = manageDocks <+> manageHook xfceConfig
-    , layoutHook = avoidStruts $ layoutHook xfceConfig
-    , terminal = "xfce4-terminal"
+      manageHook = myManageHook
+    , layoutHook = myLayoutHook
+    , workspaces = myWorkspaces
+    , terminal = myTerminal
     , modMask = mod4Mask
-    , borderWidth = 0
-    }
+    , borderWidth = 2
+    } `additionalKeys` myKeys
+
+myManageHook = manageDocks <+> manageHook xfceConfig
+
+myLayoutHook = avoidStruts $
+               gaps [(U,5),(L,5),(R,5),(D,5)] $
+               spacing 5 $
+               layoutHook xfceConfig
+
+myWorkspaces = [show x | x <- [1..9]]
+
+myKeys =
+  [
+    ((mod1Mask, xK_Tab), windows W.focusDown)
+  , ((mod4Mask, xK_b), moveTo Prev NonEmptyWS)
+  , ((mod4Mask, xK_f), moveTo Next NonEmptyWS)
+  ]
+
+myTerminal = "xfce4-terminal"
